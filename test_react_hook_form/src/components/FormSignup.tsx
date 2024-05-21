f7278ff31f177afc8591bc55936f6b35a52edbcc
cdev <cuong170603@gmail.com>
2024-05-21 08:53:08 +0700

    docs: unit test cho form sign up

--- test_react_hook_form/src/components/FormSignup.tsx
+++ test_react_hook_form/src/components/FormSignup.tsx
@@ -1,96 +1,134 @@
-import { FieldValues, useForm } from 'react-hook-form';
+import { FieldValues, useForm } from "react-hook-form";
 
 type FormSignupValues = {
-    name: string;
-    email: string;
-    age: number;
+  name: string;
+  email: string;
+  age: number;
 };
+
+const FormSignupOptions = {
+  name: {
+    required: {
+      value: true,
+      message: "Please enter a name",
+    },
+  },
+  email: {
+    required: {
+      value: true,
+      message: "Please enter your email",
+    },
+    validate: (value: string) => {
+      const regex = new RegExp(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+      return regex.test(value) || "Email is not valid";
+    },
+  },
+  age: {
+    required: {
+      value: true,
+      message: "Please enter a age",
+    },
+    validate: (value: number) => {
+      const regex = new RegExp(/^\d{2}$/);
+      if (regex.test(value.toString()) && 18 <= value && value <= 65) {
+        return true;
+      }
+      return "Age must be a number between 18 and 65";
+    },
+  },
+};
+
 function FormSignup() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        reset,
-    } = useForm<FormSignupValues>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormSignupValues>();
 
-    const onSubmit = async (data: FieldValues) => {
-        // Call api to server
-        console.log(data);
-        alert('Đăng ký thành công');
-        reset();
-    };
+  const onSubmit = async (data: FieldValues) => {
+    // Call api to server
+    console.log(data);
+    // alert("Đăng ký thành công");
+    reset();
+  };
 
-    return (
-        <form className='form-signup border rounded' onSubmit={handleSubmit(onSubmit)}>
-            <h2 className='form-title text-primary my-4 border-bottom pb-3'>Đăng ký</h2>
-            <div className='form-body p-4'>
-                <div className='form-group form-inline'>
-                    <label htmlFor='' className='form-label col-lg-2 justify-content-start'>
-                        Name:
-                    </label>
-                    <input
-                        type='text'
-                        className={`form-control col-lg-10 ${errors.name ? 'invalid' : ''}`}
-                        {...register('name', {
-                            required: {
-                                value: true,
-                                message: 'Please enter a name',
-                            },
-                        })}
-                    />
-                    {errors.name && <p className='text-danger my-2 mb-0 col-lg-12'>{errors.name.message}</p>}
-                </div>
-                <div className='form-group form-inline'>
-                    <label htmlFor='' className='form-label col-lg-2 justify-content-start'>
-                        Email:
-                    </label>
-                    <input
-                        type='text'
-                        className={`form-control col-lg-10 ${errors.email ? 'invalid' : ''}`}
-                        {...register('email', {
-                            required: {
-                                value: true,
-                                message: 'Please enter your email',
-                            },
-                            validate: value => {
-                                const regex = new RegExp(
-                                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                                );
-                                return regex.test(value) || 'Email is not valid';
-                            },
-                        })}
-                    />
-                    {errors.email && <p className='text-danger my-2 mb-0 col-lg-12'>{errors.email.message}</p>}
-                </div>
-                <div className='form-group form-inline'>
-                    <label htmlFor='' className='form-label col-lg-2 justify-content-start'>
-                        Age:
-                    </label>
-                    <input
-                        type='text'
-                        className={`form-control col-lg-10 ${errors.age ? 'invalid' : ''}`}
-                        {...register('age', {
-                            required: {
-                                value: true,
-                                message: 'Please enter a age',
-                            },
-                            validate: value => {
-                                const regex = new RegExp(/^\d{2}$/);
-                                if (regex.test(value.toString()) && 18 <= value && value <= 65) {
-                                    return true;
-                                }
-                                return 'Age must be a number between 18 and 65';
-                            },
-                        })}
-                    />
-                    {errors.age && <p className='text-danger my-2 mb-0 col-lg-12'>{errors.age.message}</p>}
-                </div>
-                <div className='form-group d-flex justify-content-center'>
-                    <button className='btn btn-primary form-button mr-2'>Đăng ký</button>
-                </div>
-            </div>
-        </form>
-    );
+  return (
+    <form
+      className="form-signup border rounded"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <h2 className="form-title text-primary my-4 border-bottom pb-3">
+        Sign up
+      </h2>
+      <div className="form-body p-4">
+        <div className="form-group form-inline">
+          <label
+            htmlFor="name"
+            className="form-label col-lg-2 justify-content-start"
+          >
+            Name:
+          </label>
+          <input
+            id="name"
+            type="text"
+            className={`form-control col-lg-10 ${errors.name ? "invalid" : ""}`}
+            {...register("name", FormSignupOptions.name)}
+          />
+          {errors.name && (
+            <p className="text-danger my-2 mb-0 col-lg-12">
+              {errors.name.message}
+            </p>
+          )}
+        </div>
+        <div className="form-group form-inline">
+          <label
+            htmlFor="email"
+            className="form-label col-lg-2 justify-content-start"
+          >
+            Email:
+          </label>
+          <input
+            id="email"
+            type="text"
+            className={`form-control col-lg-10 ${
+              errors.email ? "invalid" : ""
+            }`}
+            {...register("email", FormSignupOptions.email)}
+          />
+          {errors.email && (
+            <p className="text-danger my-2 mb-0 col-lg-12">
+              {errors.email.message}
+            </p>
+          )}
+        </div>
+        <div className="form-group form-inline">
+          <label
+            htmlFor="age"
+            className="form-label col-lg-2 justify-content-start"
+          >
+            Age:
+          </label>
+          <input
+            id="age"
+            type="text"
+            className={`form-control col-lg-10 ${errors.age ? "invalid" : ""}`}
+            {...register("age", FormSignupOptions.age)}
+          />
+          {errors.age && (
+            <p className="text-danger my-2 mb-0 col-lg-12">
+              {errors.age.message}
+            </p>
+          )}
+        </div>
+        <div className="form-group d-flex justify-content-center">
+          <button className="btn btn-primary form-button mr-2">Sign up</button>
+        </div>
+      </div>
+    </form>
+  );
 }
 
 export default FormSignup;
---
+++ test_react_hook_form/src/tests/FormSignup.test.tsx
@@ -0,0 +1,92 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import FormSignup from "../components/FormSignup";
+import "@testing-library/jest-dom";
+
+describe("Test form sign up", () => {
+  test("renders form correctly", () => {
+    // Hiển thị component FormSignup
+    render(<FormSignup />);
+    // Kiểm tra xem có button submit trên form không?
+    expect(screen.getByRole("button", { name: "Sign up" })).toBeInTheDocument();
+  });
+
+  test("shows validation errors when form is submitted with empty", async () => {
+    render(<FormSignup />);
+    // Lấy phần tử button submit
+    const submitButton = screen.getByRole("button", { name: "Sign up" });
+    userEvent.click(submitButton);
+
+    // Đợi component cập nhật lại state hết sau đó mới thực hiện kiểm tra kết quả test
+    await waitFor(() => {
+      expect(screen.getByText("Please enter a name")).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.getByText("Please enter your email")).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.getByText("Please enter a age")).toBeInTheDocument();
+    });
+  });
+
+  test("shows validation errors when form is submitted with invalid data", async () => {
+    // Hiển thị component FormSignup
+    render(<FormSignup />);
+
+    // Lấy phần tử button submit và các phần tử input có trong FormSignup
+    const submitButton = screen.getByRole("button", { name: "Sign up" });
+    const nameInput = screen.getByLabelText("Name:", { selector: "input" });
+    const emailInput = screen.getByLabelText("Email:", { selector: "input" });
+    const ageInput = screen.getByLabelText("Age:", { selector: "input" });
+
+    // Mô tả sự kiện người dùng change giá trị trong các trường input
+    userEvent.type(nameInput, "John Doe");
+    userEvent.type(emailInput, "johndoe.com");
+    userEvent.type(ageInput, "100");
+
+    // Mô tả sự kiện người dùng click vào button submit form
+    userEvent.click(submitButton);
+
+    // Đợi component cập nhật lại state hết sau đó mới thực hiện kiểm tra kết quả test
+    await waitFor(() => {
+      expect(screen.getByText("Email is not valid")).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(
+        screen.getByText("Age must be a number between 18 and 65")
+      ).toBeInTheDocument();
+    });
+  });
+
+  test("submits form correctly when valid data is provided", async () => {
+    // Hiển thị component FormSignup
+    render(<FormSignup />);
+
+    // Lấy phần tử button submit và các phần tử input có trong FormSignup
+    const submitButton = screen.getByRole("button", { name: "Sign up" });
+    const nameInput = screen.getByLabelText("Name:", { selector: "input" });
+    const emailInput = screen.getByLabelText("Email:", { selector: "input" });
+    const ageInput = screen.getByLabelText("Age:", { selector: "input" });
+
+    // Mô tả sự kiện người dùng change giá trị trong các trường input
+    userEvent.type(nameInput, "John Doe");
+    userEvent.type(emailInput, "johndoe@example.com");
+    userEvent.type(ageInput, "30");
+
+    // Mô tả sự kiện người dùng click vào button submit form
+    userEvent.click(submitButton);
+
+    // Đợi component cập nhật lại state hết sau đó mới thực hiện kiểm tra kết quả test
+    await waitFor(() => {
+      expect(screen.queryByText("Please enter a name")).not.toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(
+        screen.queryByText("Please enter your email")
+      ).not.toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.queryByText("Please enter a age")).not.toBeInTheDocument();
+    });
+  });
+});
